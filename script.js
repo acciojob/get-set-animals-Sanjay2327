9a52561f58c85b49aa8094e2632b09e1e437d27b
Sanjay Kushwaha <sanjaykushwaha80676@gmail.com>
2026-09-13 19:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	get species(){return this.species};
 
-class Dog extends Animal {}
+	makeSound(){
+		console.log("The "+this.species+"makes a sound" );
+	}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	purr(){
+		console.log("purr");
+	}
+}
 
+class Cat extends Animal {
+	bark(){
+		console.log("woof");
+	}
+}
+
+const myCat = new Cat("Siamese");
+myCat.makeSound();
+
+myCat.purr();
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound();
+
+myDog.bark();
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
